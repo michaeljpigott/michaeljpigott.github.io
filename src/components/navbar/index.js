d459d1436db9f9a9c6382e95f04184813fe59554
michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>
2024-03-20 15:40:24 +1100

    Finished off changes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,16 @@
 // Filename - "./components/Navbar.js
 
 import React from "react";
-import { Nav, NavLink, NavMenu } from "./NavbarElements";
+import { Nav, NavLink, NavMenu } from "./navbarElements";
 
 const Navbar = () => {
 	return (
 		<>
 			<Nav>
 				<NavMenu>
+        <NavLink to="/" activeStyle>
+						Home
+					</NavLink>
 					<NavLink to="/about" activeStyle>
 						About
 					</NavLink>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 // Filename - pages/index.js
 
 import React from "react";
+import Bootstrap from "bootstrap";
 
 const Home = () => {
 	return (
-		<div>
+		<div class="container-lg">
 			<h1>Welcome to my test website</h1>
+      <a href="https://google.com.au/"><button type="button" class="btn btn-lg btn-primary">Big button</button></a>
 		</div>
+
 	);
 };
 
