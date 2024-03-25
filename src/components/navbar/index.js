47677763964c68a2eafc8b83bf7ed795a30d649e
michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>
2024-03-25 15:55:19 +1100

    Fixed about link in navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -20,7 +20,7 @@ const Navigation = () => {
       <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
         <Nav.Link as={Link} to="/">Home</Nav.Link>
-        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link as={Link} to="/about">About</Nav.Link>
         <Nav.Link href="/blogs">Blogs</Nav.Link>
         <Nav.Link href="/contact">Contact</Nav.Link>
        <Nav.Link href="/sign-up">Sign up</Nav.Link>
