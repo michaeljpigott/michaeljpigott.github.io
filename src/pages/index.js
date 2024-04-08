94e67f6644b8de4873131f0b80df3987299ab0b8
michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>
2024-04-08 15:30:23 +1000

    Added some personal information to test Clarity's redaction feature.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const Home = () => {
         <meta name="The homepage for the test site." />
       </Helmet>
 			<h1>Welcome to my test website</h1>
+      <p>Random text goes here. Joe Bloggs is a good guy. I like him. He lives at 102 Smith Street in Melbourne. He was born on 1 January 1901.  </p>
       <a href="https://google.com.au/"><button type="button" class="btn btn-lg btn-primary">Big button</button></a>
 		</div>
 
